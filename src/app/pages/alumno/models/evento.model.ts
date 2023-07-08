ce6932153dc59ec9ea5f4139a1084f865d941490
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-07-08 16:56:49 +0200

    Calendario

---
+++ src/app/pages/alumno/models/evento.model.ts
@@ -0,0 +1,9 @@
+export interface Evento {
+  id: number;
+  titulo: string;
+  descripcion: string;
+  start: string; // Agrega la propiedad start de tipo string
+  end: string; // Agrega la propiedad end de tipo string
+  fecha: string;
+  color: string;
+}
