d49620cf4444c30412fc8ec29859fb7b2e0810d2
martinencina <mnencina@gmail.com>
2023-07-10 23:23:09 +0200

    page profesores pendiente post/put de notas

--- src/app/pages/profesor/asignatura-profesor/asignatura-profesor.component.ts
+++ src/app/pages/profesor/asignatura-profesor/asignatura-profesor.component.ts
@@ -13,6 +13,7 @@ export class AsignaturaProfesorComponent implements OnInit {
   plantillaAlumnos: any[] = [];
   notas: any[] = [];
   asignatura: string= ""
+  asignaturaActiva: string= ""
   constructor(
     private servicio: ProfesorServiceService,
     private router: Router,
@@ -26,21 +27,35 @@ export class AsignaturaProfesorComponent implements OnInit {
       this.nuevoArray = data.filter((asignatura: { profesor: string[] }) => asignatura.profesor.includes(this.user._id));
       console.log(this.nuevoArray);
     });
+    
   }
   
-  plantillaAsignatura(id: string): void {
-  this.asignatura= id 
+cambiarAsignatura(index: number): void { // Método para cambiar la asignatura activa
+  this.asignaturaActiva = this.nuevoArray[index]._id;
+}
+
+plantillaAsignatura(id: string): void {
+  this.asignatura = id;
   this.servicio.getAlumnos().subscribe((data: any) => {
-    this.plantillaAlumnos = data.filter((alumno: { asignaturas: string[] }) => alumno.asignaturas.includes(id));
+    this.plantillaAlumnos = data.filter((alumno: { asignaturas: string[] }) =>
+      alumno.asignaturas.includes(id)
+    );
     console.log(this.plantillaAlumnos);
-});
 
-} 
+    // Obtener las notas de los alumnos
+    this.servicio.getNotas().subscribe((notasData: any) => {
+      this.notas = notasData.filter(
+        (nota: { alumno: string[]; asignatura: string[] }) =>
+          nota.asignatura.includes(id) &&
+          this.plantillaAlumnos.some((alumno) =>
+            nota.alumno.includes(alumno._id)
+          )
+      );
+      console.log(this.notas);
+    });
+  });
+}
 
-verificarnotas(id: string ): void {this.servicio.getNotas().subscribe((data: any) =>{
-  this.notas= data.filter((nota: { alumno: string[], asignatura:string }) => nota.asignatura.includes(this.asignatura) && nota.alumno.includes(id) );
-  console.log(this.notas);
-});
 
-} ;
+
 }
